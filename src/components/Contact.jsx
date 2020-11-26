2176ae477e3a69c4c744378a313627f31c732c42
richachauhan59 <richachauhan2019@gmail.com>
2020-11-27 01:29:49 +0530

    Final 1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Heading = styled.h1`
+ text-align: center;
+ font-family: 'Ubuntu', sans-serif;
+ font-size: 40px;
+ color: white;
+`;
+
+const Line = styled.div`
+  height: 4px;
+  width: 125px;
+  margin: 10px auto;
+  background: white;
+`;
 
 function Contact() {
  return (
     <footer>
     <div className="container-fluid page-footer font-small text-white text-center p-5"
         style={{backgroundImage: "linear-gradient(361deg, rgb(35, 39, 65), rgb(10 86 73 / 86%))", height: "360px"}}>
-        <p className="h2">Get in Touch</p>
+        <Heading id="contacts">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            data-aos-easing="ease-out-back"
+          >
+            Get in Touch
+          </div>
+          <Line
+            data-aos="fade-left"
+            data-aos-duration="1500"
+            data-aos-easing="ease-out-back"
+          />
+        </Heading>
 
         <div className="row p-3">
 
@@ -30,10 +58,10 @@ function Contact() {
 
             <div id="contact" className="col-md-12 pb-3" style={{height:"70px"}}>
                 <div className="flex-center">
-                    <a href="#" target="_blank" className="fas fa-map-marker text-white p-4 fa-1x">Bangalore</a>
-                    <a href="#richachauhan2019@gmail.com" className="fas fa-envelope-open text-white p-4 fa-1x"
+                    <a href="#" target="_blank" className="h4 fas fa-map-marker text-white p-4 fa-1x">Bangalore</a>
+                    <a href="#richachauhan2019@gmail.com" className="h4 fas fa-envelope-open text-white p-4 fa-1x"
                         target="_blank">richachauhan2019@gmail.com</a>
-                    <a href="#" target="_blank" className="fa fa-phone text-white p-4 fa-1x">8130700305</a>
+                    <a href="#" target="_blank" className="h4 fa fa-phone text-white p-4 fa-1x">8130700305</a>
                 </div>
             </div>
 
