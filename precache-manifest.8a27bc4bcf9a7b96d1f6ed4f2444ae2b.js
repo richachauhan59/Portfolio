9b64eb125eee3dc15b2e1179e6de4ef7bc9ebbcb
richachauhan59 <richachauhan2019@gmail.com>
2020-11-26 21:13:16 +0530

    Updates

---
+++ precache-manifest.8a27bc4bcf9a7b96d1f6ed4f2444ae2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c9ee47adc1be3a8eb5b695590d0a3d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1a9f912d99189714833b",
+    "url": "/portfolio/static/js/2.2d06e864.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.2d06e864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80743dfe64910e9f64b3",
+    "url": "/portfolio/static/js/main.d9977d50.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
